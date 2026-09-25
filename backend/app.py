d5cdc119a48a77e8648af2d8cb53442c1ad27b11
S S Apoorva <ssapoorva1216@gmail.com>
2026-09-25 20:08:01 +0530

    Prepare backend for deployment

--- backend/app.py
+++ backend/app.py
@@ -567,10 +567,9 @@ def predict():
 # RUN FLASK
 # ============================================================
 
-if __name__ == "__main__":
+    import os
 
-    app.run(
-        debug=True,
-        host="127.0.0.1",
-        port=5000
-    )
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)   
+ )
